cf57b83bd00558c604eac4b32a44df60cf31add3
YashGarg-4 <yashgarg04@gmail.com>
2024-12-19 11:47:20 +0530

    Props

---
+++ Learning props/App.jsx
@@ -0,0 +1,22 @@
+import Content from "./contents"
+export default function App() {
+    return (
+    <>
+        <Content 
+        name = "One"
+        age = "1"
+        Gender = "M"
+        />
+        <Content 
+        name = "Two"
+        age = "2"
+        Gender = "M"
+        />
+        <Content 
+        name = "3"
+        age = "3"
+        Gender = "F"
+        />
+    </>
+    )
+}
---
+++ Learning props/contents.jsx
@@ -0,0 +1,15 @@
+export default function Content(props){
+    return (
+        <>
+            <div>
+                <h1>{props.name}</h1>
+            </div>
+            <div>
+                <h1>{props.age}</h1>
+            </div>
+            <div>
+                <h1>{props.Gender}</h1>
+            </div>
+        </>
+    )
+}
